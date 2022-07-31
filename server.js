ea29d6e8391ccd16f610c85c1baca4b811c9ff7e
boujrada yassine <63125972+yassineboujrada@users.noreply.github.com>
2022-07-31 21:37:51 +0100

    server function

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'centre.declaration@gmail.com',
-    pass: 'rcrtbuvmjyxkofcj'
+    user: '###@gmail.com',
+    pass: '#####'
   },
 });
 
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "yassine.boujrada@gmail.com",
+    to: "#####@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
